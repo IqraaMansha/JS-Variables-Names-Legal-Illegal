1a600e508e3c03c67bc14693eefdffaf2090c857
Iqra Mansha <iqramansha78u@gmail.com>
2023-05-04 13:08:04 +0500

    First Commit

---
+++ Rules.Js
@@ -0,0 +1,17 @@
+
+// Using Console
+console.log("#4 VARIABLE NAMES: LEGAL & ILLEGAL");
+
+
+//  #4 VARIABLE NAMES: LEGAL & ILLEGAL
+
+var rules = "Rules of Variables in Javascript ...";
+alert(rules);
+
+document.write
+    (' <br> Variable names can only contain . numbers , $ and _. For example $my_1stVariable. <br> ' +
+        ' <br> Variables must begin with a letter, $ or _ . For example $name, _name or name. <br> ' +
+        ' <br> Variable names are case sensitive . <br> ' +
+        ' <br> Variable names should not be JS Keywords . <br> ' +
+        '<br> A variabl name can not contain any spaces . <br> <br>'  +
+        "--END--") ;
